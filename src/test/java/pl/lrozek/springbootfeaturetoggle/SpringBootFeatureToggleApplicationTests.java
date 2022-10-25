483c1707199a04d8a2b601042fe91c5219e60d86
Łukasz Rożek <lukasz.rozek@consileon.pl>
2022-10-25 10:54:44 +0200

    bootstrap

---
+++ src/test/java/pl/lrozek/springbootfeaturetoggle/SpringBootFeatureToggleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lrozek.springbootfeaturetoggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeatureToggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
